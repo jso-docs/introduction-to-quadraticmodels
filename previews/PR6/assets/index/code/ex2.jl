e8b46c8ab70f0c783b32236bf603cac8a4cd747a
abelsiqueira <abelsiqueira@users.noreply.github.com>
2022-03-08 23:29:07 +0000

    Deploying to gh-preview from  @ f4d5be49bc3449e779a318a0a1062e5c188dadfb 🚀

---
+++ previews/PR6/assets/index/code/ex2.jl
@@ -0,0 +1,3 @@
+# This file was generated, do not modify it. # hide
+qmCOO = QuadraticModel(c, HCOO, A = ACOO, lcon = lcon, ucon = ucon, lvar = l, uvar = u,
+                       c0 = 0.0, name = "QM_COO")
