7bf1e5606d211f96380910b7c6e0db2828826d71
abelsiqueira <abelsiqueira@users.noreply.github.com>
2022-03-08 22:41:04 +0000

    Deploying to gh-preview from  @ fd4026f851002292d28eca285d1383306bb807f4 🚀

---
+++ previews/PR4/assets/index/code/ex10.jl
@@ -0,0 +1,4 @@
+# This file was generated, do not modify it. # hide
+using QPSReader
+qps = readqps("AFIRO.SIF")
+qmCOO4 = QuadraticModel(qps)
---
+++ previews/PR4/assets/index/code/ex4.jl
@@ -0,0 +1,4 @@
+# This file was generated, do not modify it. # hide
+qmLinop = QuadraticModel(c, LinearOperator(Symmetric(H)), A = LinearOperator(A),
+                         lcon = lcon, ucon = ucon, lvar = l, uvar = u,
+                         c0 = 0.0, name = "QM_Linop")
